06b271c450909205c8aa981ea89526918d4b8bec
Anja Thormann <anja@ebi.ac.uk>
2015-07-15 14:34:11 +0100

    add consequence_colors

--- cgi-bin/tmpl_handler.pl
+++ cgi-bin/tmpl_handler.pl
@@ -93,7 +93,46 @@ my $constants = {
     msg => 'Successfully updated organ list.',
     type => 'success',
   }
+};
 
+my $consequence_colors = {
+  'intergenic_variant'                 => '#636363',
+  'intron_variant'                     => '#02599c',
+  'upstream_gene_variant'              => '#91a2b8',
+  'downstream_gene_variant'            => '#91a2b8',
+  '5_prime_UTR_variant'                => '#7ac5cd',
+  '3_prime_UTR_variant'                => '#7ac5cd',
+  'splice_region_variant'              => '#ff7f50',
+  'splice_donor_variant'               => '#FF581A',
+  'splice_acceptor_variant'            => '#FF581A',
+  'frameshift_variant'                 => '#9400D3',
+  'transcript_ablation'                => '#ff0000',
+  'transcript_amplification'           => '#ff69b4',
+  'inframe_insertion'                  => '#ff69b4',
+  'inframe_deletion'                   => '#ff69b4',
+  'synonymous_variant'                 => '#76ee00',
+  'stop_retained_variant'              => '#76ee00',
+  'missense_variant'                   => '#d8b600',
+  'start_lost'                         => '#ffd700',
+  'stop_gained'                        => '#ff0000',
+  'stop_lost'                          => '#ff0000',
+  'mature_mirna_variant'               => '#99FF00',
+  'non_coding_transcript_exon_variant' => '#32cd32',
+  'non_coding_transcript_variant'      => '#32cd32',
+  'no_consequence'                     => '#68228b',
+  'incomplete_terminal_codon_variant'  => '#ff00ff',
+  'nmd_transcript_variant'             => '#ff4500',
+  'hgmd_mutation'                      => '#8b4513',
+  'coding_sequence_variant'            => '#99FF00',
+  'failed'                             => '#cccccc',
+  'tfbs_ablation'                      => '#a52a2a',
+  'tfbs_amplification'                 => '#a52a2a',
+  'tf_binding_site_variant'            => '#a52a2a',
+  'regulatory_region_variant'          => '#a52a2a',
+  'regulatory_region_ablation'         => '#a52a2a',
+  'regulatory_region_amplification'    => '#a52a2a',
+  'protein_altering_variant'           => '#FF0080',
+  'NMD_transcript_variant'             => '#007fff',
 };
 
 sub show_downloads_page {
@@ -478,7 +517,9 @@ sub get_variations {
   }
   my @array = ();
   while (my ($consequence, $count) = each %$counts) {
-    push @array, {'label' => $consequence, 'value' => $count};
+    push @array, {'label' => $consequence, 'value' => $count, 'color' => $consequence_colors->{$consequence} || '#d0d6fe'};
+#    push @array, {'label' => $consequence, 'value' => $count};
+
   }
   my $encoded_counts = encode_json(\@array);
 
