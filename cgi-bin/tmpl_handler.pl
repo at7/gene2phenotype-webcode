1d4d4af012b56729cf95611dab55a13a067fa5da
Anja Thormann <anja@ebi.ac.uk>
2015-07-16 14:38:57 +0100

    update constants

--- cgi-bin/tmpl_handler.pl
+++ cgi-bin/tmpl_handler.pl
@@ -25,37 +25,37 @@ my $configuration_file = '../../../../config/registry';
 
 my $registry = G2P::Registry->new($configuration_file);
 
-my @constants = (
-  { text => 'RESET_PWD_SUC', msg => 'Password was successfully updated.', type => 'success'}, 
-  { text => 'PWD_ERROR', msg  => 'Error. Password verification failed.', type => 'danger',},
-  { text => 'PWDS_DONT_MATCH', msg => 'Error. Retyped and new password don\'t match.', type => 'danger',},
-  { text => 'MISSING_PWDS', msg => 'Error. You must provide a new password and retype the new password.', type => 'danger',},
-  { text => 'RESET_PWD_ERROR', msg => 'There was an error resetting you password. Please contact g2p-help@ebi.ac.uk.', type => 'danger'},
-  { text => 'RESET_USERNAME_SUC', msg => 'Username was successfully updated.', type => 'success',},
-  { text => 'USERNAME_IN_USE', msg => 'The new username is already taken.', type => 'danger'},
-  { text => 'NEW_USERNAME_MISSING', msg => 'You need to provide a new username.', type => 'danger',},
-  { text => 'EMAIL_IN_USE', msg => 'The new email is already taken.', type => 'danger',},
-  { text => 'RESET_EMAIL_SUC', msg => 'Email was successfully updated.', type => 'success',},
-  { text => 'EMAIL_UNKNOWN', msg => 'The email address is not known. Please contact g2p-help@ebi.ac.uk.', type => 'danger',},
-  { text => 'SESSION_IDS_DONT_MATCH', msg => 'Session ids don\'t match. Please contact g2p-help@ebi.ac.uk.', type => 'danger',},
-  { text => 'ERROR_ADD_GENE_DISEASE_PAIR', msg => 'You must provide a gene name and a disease name.', type => 'danger',},  
-  { text => 'LOGIN_FAILED', msg => 'Login failed. You entered a wrong password. Try again or reset your password.', type => 'danger',},
-  { text => 'DISEASE_NAME_IN_DB', msg => 'Disease name is already in database.', type => 'danger',},
-  { text => 'UPDATED_DISEASE_ATTRIBS_SUC', msg => 'Successfully updated disease attributes.', type => 'success',},
-  { text => 'UPDATED_VISIBILITY_STATUS_SUC', msg => 'Successfully updated visibility status.', type => 'success',},
-  { text => 'DISEASE_MIM_IN_DB', msg => 'Disease mim is already in database.', type => 'danger',},
-  { text => 'WRONG_FORMAT_DISEASE_MIM', msg => 'Invalid format for disease mim. It needs to be a number.', type => 'danger',},
-  { text => 'UPDATED_ORGAN_LIST', msg => 'Successfully updated organ specificity list.', type => 'success',},
-  { text => 'UPDATED_DDD_CATEGORY_SUC', msg => 'Successfully updated DDD category', type => 'success',},
-  { text => 'UPDATED_GFD_ACTION_SUC', msg => 'Successfully updated genomic feature disease action.', type => 'success',},
-  { text => 'ADDED_GFD_ACTION_SUC', msg => 'Successfully added a new genomic feature disease action.', type => 'success'},
-  { text => 'ADDED_GFDPHENOTYPE_SUC', msg => 'Successfully added a new phenotype for the genomic feature disease pair.', type => 'success'},
-  { text => 'ADDED_PUBLICATION_SUC', msg => 'Successfully added a new publication', type => 'success'},
-  { text => 'DELETED_GFDPHENOTYPE_SUC', msg => 'Successfully delete a phenotype entry.', type => 'success'},
-  { text => 'ADDED_GFDPC_SUC', msg => 'Successfully added a new comment.', type => 'success'},
-  { text => 'DELETED_GFD_ACTION_SUC', msg => 'Successfully deleted a genomic feature disease action.', type => 'success'},
-  { text => 'DELETED_GFDPC_SUC', msg => 'Successfully deleted the comment.', type => 'success'},
-);
+my $constants = {
+  RESET_PWD_SUC => { msg => 'Password was successfully updated.', type => 'success'}, 
+  PWD_ERROR => { msg  => 'Error. Password verification failed.', type => 'danger',},
+  PWDS_DONT_MATCH => { msg => 'Error. Retyped and new password don\'t match.', type => 'danger',},
+  MISSING_PWDS => { msg => 'Error. You must provide a new password and retype the new password.', type => 'danger',},
+  RESET_PWD_ERROR => { msg => 'There was an error resetting you password. Please contact g2p-help@ebi.ac.uk.', type => 'danger'},
+  RESET_USERNAME_SUC => { msg => 'Username was successfully updated.', type => 'success',},
+  USERNAME_IN_USE => { msg => 'The new username is already taken.', type => 'danger'},
+  NEW_USERNAME_MISSING => { msg => 'You need to provide a new username.', type => 'danger',},
+  EMAIL_IN_USE => { msg => 'The new email is already taken.', type => 'danger',},
+  RESET_EMAIL_SUC => { msg => 'Email was successfully updated.', type => 'success',},
+  EMAIL_UNKNOWN => { msg => 'The email address is not known. Please contact g2p-help@ebi.ac.uk.', type => 'danger',},
+  SESSION_IDS_DONT_MATCH => { msg => 'Session ids don\'t match. Please contact g2p-help@ebi.ac.uk.', type => 'danger',},
+  ERROR_ADD_GENE_DISEASE_PAIR => { msg => 'You must provide a gene name and a disease name.', type => 'danger',},  
+  LOGIN_FAILED => { msg => 'Login failed. You entered a wrong password. Try again or reset your password.', type => 'danger',},
+  DISEASE_NAME_IN_DB => { msg => 'Disease name is already in database.', type => 'danger',},
+  UPDATED_DISEASE_ATTRIBS_SUC => { msg => 'Successfully updated disease attributes.', type => 'success',},
+  UPDATED_VISIBILITY_STATUS_SUC => { msg => 'Successfully updated visibility status.', type => 'success',},
+  DISEASE_MIM_IN_DB => { msg => 'Disease mim is already in database.', type => 'danger',},
+  WRONG_FORMAT_DISEASE_MIM => { msg => 'Invalid format for disease mim. It needs to be a number.', type => 'danger',},
+  UPDATED_ORGAN_LIST => { msg => 'Successfully updated organ specificity list.', type => 'success',},
+  UPDATED_DDD_CATEGORY_SUC => { msg => 'Successfully updated DDD category', type => 'success',},
+  UPDATED_GFD_ACTION_SUC => { msg => 'Successfully updated genomic feature disease action.', type => 'success',},
+  ADDED_GFD_ACTION_SUC => { msg => 'Successfully added a new genomic feature disease action.', type => 'success'},
+  ADDED_GFDPHENOTYPE_SUC => { msg => 'Successfully added a new phenotype for the genomic feature disease pair.', type => 'success'},
+  ADDED_PUBLICATION_SUC => { msg => 'Successfully added a new publication', type => 'success'},
+  DELETED_GFDPHENOTYPE_SUC => { msg => 'Successfully delete a phenotype entry.', type => 'success'},
+  ADDED_GFDPC_SUC => { msg => 'Successfully added a new comment.', type => 'success'},
+  DELETED_GFD_ACTION_SUC => { msg => 'Successfully deleted a genomic feature disease action.', type => 'success'},
+  DELETED_GFDPC_SUC => { msg => 'Successfully deleted the comment.', type => 'success'},
+};
 
 my $consequence_colors = {
   'intergenic_variant'                 => '#636363',
@@ -97,11 +97,9 @@ my $consequence_colors = {
   'NMD_transcript_variant'             => '#007fff',
 };
 
-
 sub get_message_hash {
   my $text = shift;
-  my $index =  first_index { $_->{text} eq $text } @constants; 
-  return $constants[$index];
+  return $constants->{$text};
 }
 
 sub show_downloads_page {
